6da8972fae5e681bebcd113e0c7c124a592160a0
cjv380 <168764245+cjv380@users.noreply.github.com>
2025-09-25 22:31:37 -0500

    web-server-4

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,4 @@
 terraform {
-  cloud {
-    organization = "chrisv-org"  # Replace with your HCP Terraform organization name
-
-    workspaces {
-      name = "Deploy-a-web-server-on-aws-vcs-workflow"  # Replace with your workspace name
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
